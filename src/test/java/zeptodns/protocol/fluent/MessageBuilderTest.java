d288a913301b23b97bbcb9501047f645b8c7ce34
Rupert Muchembled <rupert@rmuch.net>
2014-03-24 17:15:28 +0000

    Test MessaeTypeStep

--- src/test/java/zeptodns/protocol/fluent/MessageBuilderTest.java
+++ src/test/java/zeptodns/protocol/fluent/MessageBuilderTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import zeptodns.protocol.messages.Message;
+import zeptodns.protocol.wire.FlagUtils;
+
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class MessageBuilderTest {
@@ -14,6 +17,16 @@ public class MessageBuilderTest {
                 .asQuery()
                 .end();
 
-        // TODO
+        assertEquals(FlagUtils.getQueryResponse(message.getHeaderSection().getFlags()), 0);
+    }
+
+    @Test
+    public void thatMessageBuilderResponseIsValid() {
+        Message message = MessageBuilder
+                .begin()
+                .asResponse()
+                .end();
+
+        assertEquals(FlagUtils.getQueryResponse(message.getHeaderSection().getFlags()), 1);
     }
 }
