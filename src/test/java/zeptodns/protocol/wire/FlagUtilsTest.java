cbf5014e5d768cf04b5fe74cc0eadc6ce3691acc
Rupert Muchembled <rupert@rmuch.net>
2014-03-25 16:14:25 +0000

    Add test case for overlapping flags

--- src/test/java/zeptodns/protocol/wire/FlagUtilsTest.java
+++ src/test/java/zeptodns/protocol/wire/FlagUtilsTest.java
@@ -20,4 +20,29 @@ public class FlagUtilsTest {
 
         assertEquals(flags, 0);
     }
+
+    @Test
+    public void thatFlagsSetAndOverlapCorrectly() {
+        int flags = 0;
+
+        flags = FlagUtils.setQueryResponse(flags, 1);
+        flags = FlagUtils.setOpcode(flags, 2);
+        flags = FlagUtils.setAuthoritative(flags, 1);
+        flags = FlagUtils.setTruncation(flags, 0);
+        flags = FlagUtils.setRecursionDesired(flags, 0);
+        flags = FlagUtils.setRecursionAvailable(flags, 1);
+        flags = FlagUtils.setReserved(flags, 2);
+        flags = FlagUtils.setResponseCode(flags, 1);
+
+        assertEquals(flags, 5413);
+
+        assertEquals(FlagUtils.getQueryResponse(flags), 1);
+        assertEquals(FlagUtils.getOpcode(flags), 2);
+        assertEquals(FlagUtils.getAuthoritative(flags), 1);
+        assertEquals(FlagUtils.getTruncation(flags), 0);
+        assertEquals(FlagUtils.getRecursionDesired(flags), 0);
+        assertEquals(FlagUtils.getRecursionAvailable(flags), 1);
+        assertEquals(FlagUtils.getReserved(flags), 2);
+        assertEquals(FlagUtils.getResponseCode(flags), 1);
+    }
 }
